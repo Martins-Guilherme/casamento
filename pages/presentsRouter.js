edf624439e38a999935cb9306d5631381bf292b4
Guilherme Martins <guilhermeteixeira3113@gmail.com>
2025-09-14 19:30:48 -0300

    feat: add unselect and selected routes for presents management

--- pages/presentsRouter.js
+++ pages/presentsRouter.js
@@ -33,7 +33,6 @@ router.delete("/:id", autenticarAdmin, async (req, res) => {
   }
 })
 
-
 // LISTAR PRESENTES
 router.get("/listar", autenticarAdmin, async (req, res) => {
   try {
@@ -44,6 +43,64 @@ router.get("/listar", autenticarAdmin, async (req, res) => {
   }
 })
 
+// DESMARCAR. convidadoid = null
+router.put("/unselect/:id", autenticarAdmin, async (req, res) => {
+  try {
+    const { id } = req.params;
+
+    // Encontra o presente e verifica se ele está associado a um convidado
+    const presente = await prisma.tabelaDePresentes.findUnique({
+      where: { id: parseInt(id) },
+      select: { convidadoId: true }
+    });
+
+    // Se o presente não estiver vinculado a nenhum convidado.
+    if (!presente || presente.convidadoId === null) {
+      return res.status(204).json({ error: "Este presente não foi escolhido por nenhum convidado." });
+    }
+
+    // Desvincula o presente do convidado
+    const presenteAtualizado = await prisma.tabelaDePresentes.update({
+      where: { id: parseInt(id) },
+      data: { convidadoId: null }
+    });
+
+    // Marca o token do convidado como não usado novamente
+    await prisma.convidado.update({
+      where: { id: presente.convidadoId },
+      data: { usado: false }
+    });
+
+    res.status(200).json(presenteAtualizado);
+
+  } catch (error) {
+    console.error(`Erro ao tentar desmarcar o presente ${error}`)
+    res.status(501).json({ error: "Erro interno." })
+  }
+})
+
+// Rota para dar o retorno se o presente foi selecionado pelo convidado.
+router.get("/selected/:id", async (req, res) => {
+  try {
+    const { id } = req.params;
+
+    // Encontra o presente e verifica se ele está associado a um convidado
+    const presente = await prisma.tabelaDePresentes.findUnique({
+      where: { id: parseInt(id) },
+      select: { convidadoId: true }
+    });
+
+    // Se o presente não estiver vinculado a nenhum convidado.
+    if (!presente || presente.convidadoId === null) {
+      return res.status(200).json({ selected: false });
+    }
+
+    res.status(200).json({ selected: true });
+  } catch (error) {
+    console.error(`Erro ao tentar verificar se o presente foi selecionado ${error}`)
+    res.status(501).json({ error: "Erro interno." })
+  }
+})
 
 
 export default router;
